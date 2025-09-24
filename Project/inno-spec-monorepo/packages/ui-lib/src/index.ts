edbe6cf2d8f9231d8be37b74a9c7eb603a8e09f6
jgahn38 <jgahn38@gmail.com>
2025-09-24 15:59:20 +0900

    [20250924] designer-app 불필요 파일 제거

--- Project/inno-spec-monorepo/packages/ui-lib/src/index.ts
+++ Project/inno-spec-monorepo/packages/ui-lib/src/index.ts
@@ -3,6 +3,7 @@ export { default as Button } from './components/Button';
 export { default as Input } from './components/Input';
 export { default as Card, CardHeader, CardBody, CardFooter } from './components/Card';
 export { default as Modal } from './components/Modal';
+export { default as LoginView } from './components/LoginView';
 
 // 레이아웃 컴포넌트
 export { default as Header } from './layouts/Header';
@@ -13,6 +14,7 @@ export type { ButtonProps } from './components/Button';
 export type { InputProps } from './components/Input';
 export type { CardProps, CardHeaderProps, CardBodyProps, CardFooterProps } from './components/Card';
 export type { ModalProps } from './components/Modal';
+export type { LoginViewProps } from './components/LoginView';
 export type { HeaderProps, AppType } from './layouts/Header';
 export type { SidebarProps } from './layouts/Sidebar';
 
